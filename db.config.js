ceef421aece813d4d0c14bd0b61b488afb258f3b
conner1reimers <66189525+conner1reimers@users.noreply.github.com>
2022-04-23 21:34:57 -0500

    .

--- db.config.js
+++ db.config.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 module.exports = {
-    HOST: "project-db.cy9yp9l7shrh.us-east-2.rds.amazonaws.com",
-    USER: "admin",
-    PASSWORD: "3QHEp7TP50UUuvmwbzdO",
-    DB: "testdb"
+    HOST: process.env.HOST,
+    USER: process.env.USER,
+    PASSWORD: process.env.PASSWORD,
+    DB: process.env.DB
 }
